81b426074a43d5cb0fa754dc29133bccd36a810d
yongsiangong <107787592+yongsiangong@users.noreply.github.com>
2024-07-13 16:28:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-# import logging
-# import logging.handlers
-
 import os
 import requests
 import pandas as pd
 import time
 from textblob import TextBlob
 import sqlite3
-
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-# logger_file_handler = logging.handlers.RotatingFileHandler(
-#     "status.log",
-#     maxBytes=1024 * 1024,
-#     backupCount=1,
-#     encoding="utf8",
-# )
-# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# logger_file_handler.setFormatter(formatter)
-# logger.addHandler(logger_file_handler)
-
-
 
 if __name__ == "__main__":
     API = os.environ["COINPANIC_API"]
